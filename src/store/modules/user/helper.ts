f697f519ecaac4c08a1cbe5e5ef05a0b03dbed91
jikehu <108921221+yqty@users.noreply.github.com>
2023-06-18 14:33:55 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://profile-avatar.csdnimg.cn/f4cd7f0270d0484ea3a175eabdee1ca5_weixin_64911575.jpg!1',
+      name: 'KKONE',
+      description: 'Star on <a ""</a>',
     },
   }
 }
